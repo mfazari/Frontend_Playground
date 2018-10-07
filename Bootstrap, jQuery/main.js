25631810e266b31f71ed2e26a77bd8fa596ed607
mfazari <massimofazari@bluewin.ch>
2018-10-07 15:51:47 +0200

    "JavaScript Functions updated"

--- Bootstrap, jQuery/main.js
+++ Bootstrap, jQuery/main.js
@@ -1,12 +1,13 @@
 //Variables
 var x = document.querySelector("#numero1").value;
 var y = document.querySelector("#numero2").value;
+var operator;
 var output;
 
 
 
-
-function myFunction() {
+//Checks numbers and prints out result
+function submitFunction() {
 
     //document.querySelector("#numero2").value = "42";
 
@@ -14,15 +15,15 @@ function myFunction() {
         document.querySelector("#output").innerHTML = "Invalid input";
 
     }
-}
 
-function Calc(String){
-
-
-    if(isNaN(x || y) || x.length < 1 || y.length < 1 ) {
-        document.querySelector("#output").innerHTML = "Invalid input";
+    else {
+        document.querySelector("#output").innerHTML = output;
 
     }
+
+}
+
+function Calc(operator){
     if(String == "+"){
         x = document.querySelector("#numero2").value + document.querySelector("#numero2").value;
 
