e1c6f5b305ecfb081af7f84517919dd0460d63f0
mfazari <massimofazari@bluewin.ch>
2018-10-07 15:38:16 +0200

    Progress on Playground

--- Bootstrap, jQuery/main.js
+++ Bootstrap, jQuery/main.js
@@ -1,14 +1,30 @@
+//Variables
+var x = document.querySelector("#numero1").value;
+var y = document.querySelector("#numero2").value;
+var output;
+
+
+
+
 function myFunction() {
 
-    document.querySelector("#numero2").value = "42";
+    //document.querySelector("#numero2").value = "42";
 
+    if(isNaN(x || y) || x.length < 1 || y.length < 1 ) {
+        document.querySelector("#output").innerHTML = "Invalid input";
+
+    }
 }
 
 function Calc(String){
 
+
+    if(isNaN(x || y) || x.length < 1 || y.length < 1 ) {
+        document.querySelector("#output").innerHTML = "Invalid input";
+
+    }
     if(String == "+"){
-
-
+        x = document.querySelector("#numero2").value + document.querySelector("#numero2").value;
 
     }
     document.querySelector(String).value = "42";
