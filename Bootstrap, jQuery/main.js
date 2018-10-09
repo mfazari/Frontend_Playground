a376100c6a2f04890dbd7768baba93af9ff1418d
mfazari <massimofazari@bluewin.ch>
2018-10-09 13:51:50 +0200

    jQuery added

--- Bootstrap, jQuery/main.js
+++ Bootstrap, jQuery/main.js
@@ -5,47 +5,50 @@ var currentoperator;
 var output;
 
 
-
-
 //Checks numbers and prints out result
-function submitFunction() {
+$(document).ready(function () {
+    $(document).on('submit', '#my-form', function () {
 
-    if(isNaN(x) || isNaN(y)) {
-        document.querySelector("#output").innerHTML = "Invalid input";
+        if (isNaN(x) || isNaN(y)) {
+            document.querySelector("#output").innerHTML = "Invalid input";
 
-    }
-    else if(currentoperator == null){
-        document.querySelector("#output").innerHTML = "Click on operator before submitting";
+        }
+        else if (currentoperator == null) {
+            document.querySelector("#output").innerHTML = "Click on operator before submitting";
 
-    }
-    else {
-        document.querySelector("#output").innerHTML = output;
+        }
+        else {
+            document.querySelector("#output").innerHTML = output;
 
-    }
-    console.log(output);
+        }
+        //Console
+        console.log(output);
 
-}
+    });
+});
 
-function Calc(operator){
+
+function Calc(operator) {
     currentoperator = operator;
-    if(operator === "+"){
+    if (operator === "+") {
         output = document.querySelector("#numero1").value + document.querySelector("#numero2").value;
     }
-    else if(operator === "-"){
+    else if (operator === "-") {
         output = document.querySelector("#numero1").value - document.querySelector("#numero2").value;
     }
-    else if(operator === "/"){
+    else if (operator === "/") {
         output = document.querySelector("#numero1").value * document.querySelector("#numero2").value;
 
     }
-    else if(operator === "/"){
+    else if (operator === "/") {
         output = document.querySelector("#numero1").value / document.querySelector("#numero2").value;
     }
 
-    else{
+    else {
         //donothing
 
     }
+    //Console
     console.log(currentoperator);
 
 }
